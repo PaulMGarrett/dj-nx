49c554bf55af3e64fa94069480797a77e6f14807
Paul Garrett <pgarret2@cisco.com>
2023-03-04 21:39:02 +0000

    Remove daily and meds references from settings files

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-"""meds URL Configuration
+"""mv15/nx URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('daily/', include("daily.urls")),
     path('nx/', include("nx.urls")),
     path('admin/', admin.site.urls, name='uber-admin')
 ]
